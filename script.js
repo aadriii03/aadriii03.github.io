803d38c8e656534305dd51c1071c8b1896410294
aadriii.03 <adrian.caballero2@alu.uclm.es>
2026-02-24 18:39:01 +0100

    Actualizacion de CV y fondo inicio

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ particlesJS("particles-js", {
     line_linked: {
       enable: true,
       distance: 150,
-      color: "#51080d",
+      color: "#2b0307",
       opacity: 0.4,
       width: 1
     },
